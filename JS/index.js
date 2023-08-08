40f1b3c99d5d2a9de8085bccc5a9a88928b1ddc6
sk8agustin <agustinturco5@gmail.com>
2023-08-08 10:56:45 -0300

    se borra

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+ // Función para mostrar u ocultar los párrafos al hacer clic en el botón "Ver más"
+ document.addEventListener("DOMContentLoaded", function () {
+    const verMasElements = document.querySelectorAll(".ver-mas");
+    verMasElements.forEach(function (verMas) {
+        verMas.addEventListener("click", function () {
+            const pElement = verMas.parentElement;
+            const pElements = pElement.parentElement.querySelectorAll("p");
+            pElements.forEach(function (p) {
+                p.classList.toggle("expanded");
+            });
+        });
+    });
+
+    const h2Elements = document.querySelectorAll(".Económico-Empresarial h2, .Derecho-Laboral h2, .Civil-y-comercial h2, .Societario h2, .Academia-y-consultoría h2, .Cumplimiento-normativo h2");
+    h2Elements.forEach(function (h2) {
+        h2.addEventListener("click", function () {
+            const pElement = h2.parentElement;
+            const pElements = pElement.querySelectorAll("p");
+            pElements.forEach(function (p) {
+                p.classList.toggle("expanded");
+            });
+        });
+    });
+});
